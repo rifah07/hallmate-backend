f3fe9d4fed5d2bf1326a335bee803ac68c11f8c6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-01-10 16:28:46 +0600

    environment variables added

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+// Load env variables
+dotenv.config();
+
+console.log("HallMate Backend Server");
+console.log(`Environment : ${process.env.NODE_ENV || 'development'}`);
+console.log(`Node version: ${process.version}`);
+
+const PORT = process.env.PORT || 5000;
+
+console.log(`Server is running on port: ${PORT}`);
