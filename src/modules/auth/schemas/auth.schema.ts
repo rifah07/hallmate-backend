ced14c091e3b3a2042d9739495bb19d6a4887d21
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-01-18 21:41:26 +0600

    feat: first Time Login Schema validated

--- src/modules/auth/schemas/auth.schema.ts
+++ src/modules/auth/schemas/auth.schema.ts
@@ -1,11 +1,34 @@
-import z from "zod";
+import z from 'zod';
 
 export const loginSchema = z.object({
-    body: z.object({
-        universityId: z
-            .string()
-            .min(1, 'University ID is required')
-            .regex(/d{10}$/, 'University ID must have 10 digits'),
-        password: z.string().min(1, 'Password is required'),        
-    }),
+  body: z.object({
+    universityId: z
+      .string()
+      .min(1, 'University ID is required')
+      .regex(/d{10}$/, 'University ID must have 10 digits'),
+    password: z.string().min(1, 'Password is required'),
+  }),
 });
+
+export const firstTimeLoginSchema = z
+  .object({
+    body: z.object({
+      universityId: z
+        .string()
+        .min(1, 'University ID is required')
+        .regex(/^\d{10}$/, 'University ID must be 10 digits'),
+      oneTimePassword: z.string().min(1, 'OTP is required'),
+      newPassword: z
+        .string()
+        .min(8, 'Password must be at least 8 characters')
+        .regex(/[A-Z]/, 'Password must contain at least 1 uppercase letter')
+        .regex(/[a-z]/, 'Password must contain at least 1 lowercase letter')
+        .regex(/[0-9]/, 'Password must contain at least 1 number')
+        .regex(/[@$!%*?&]/, 'Password must contain at least 1 special character'),
+      confirmPassword: z.string().min(1, 'Confirm password is required'),
+    }),
+  })
+  .refine((data) => data.body.newPassword === data.body.confirmPassword, {
+    message: 'Password do not match',
+    path: ['body', 'confirmPassword'],
+  });
