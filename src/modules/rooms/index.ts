0db1639fc528d753cabdc06b2e7018814cdb1186
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-03-26 21:46:02 +0600

    feat: Add room mudule index file (#80)

---
+++ src/modules/rooms/index.ts
@@ -0,0 +1,5 @@
+export { default as roomRoutes } from './routes/room.routes';
+export { default as roomService } from './services/room.service';
+export { default as roomController } from './controllers/room.controller';
+export { default as roomRepository } from './repositories/room.repository';
+export * from './types/room.types';
