c7e10197a80c798d5f724cea73f5e1774bb6842c
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-04-15 01:37:01 +0600

    chores: add index in applications module (#150)

---
+++ src/modules/applications/index.ts
@@ -0,0 +1,5 @@
+export { default as applicationRoutes } from './routes/application.routes';
+export { default as applicationService } from './services/application.service';
+export { default as applicationRepository } from './repositories/application.repository';
+export { default as applicationController } from './controllers/application.controller';
+export * from './types/application.types';
