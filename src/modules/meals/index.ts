d814a6c2c69bbb93c1ca09fdfc514acda2887a83
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-04-25 12:19:23 +0600

    feat: Add index file in meal module

---
+++ src/modules/meals/index.ts
@@ -0,0 +1,5 @@
+export { default as mealRoutes } from './routes/meal.routes';
+export { default as mealService } from './services/meal.service';
+export { default as mealRepository } from './repositories/meal.repository';
+export { default as mealController } from './controllers/meal.controller';
+export * from './types/meal.types';
